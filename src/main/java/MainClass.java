467af4ff9ada68fa040cbee3a664f2d175c97819
johanzilber <johanzilber@gmail.com>
2017-05-25 23:02:14 +0300

    new personal area support

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -20,7 +20,7 @@ public class MainClass {
         WebDriver driver;
         WebDriverFactory webDriverFactory = new WebDriverFactory();
         final String currentDir = System.getProperty("user.dir");
-        final File windowsChromeDriver = new File("c:" + File.separator + "_Dev" + File.separator + "chromedriver.exe");
+//        final File windowsChromeDriver = new File("c:" + File.separator + "_Dev" + File.separator + "chromedriver.exe");
         final File linuxChromeDriver = new File(File.separator + "home" + File.separator + "evegenyz" + File.separator + "yad2" + File.separator + "chromedriver");
         driver = webDriverFactory.createWebDriver(BrowserType.CHROME, linuxChromeDriver, null, null);
         driver.get("http://www.yad2.co.il/");
@@ -33,7 +33,7 @@ public class MainClass {
         try {
             System.out.println((new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(new Date()) + " STARTED");
             System.out.println((new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(new Date()) + " Going to sleep");
-//            Utils.sleepRandom(0, 60*60);
+            Utils.sleepRandom(0, 60*60);
             System.out.println((new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(new Date()) + " Wakening from sleep");
 
             driver = createWebDriver();
@@ -43,6 +43,7 @@ public class MainClass {
             Utils.sleepRandom(5, 10);
             loginArea.logIn("", "");
             personalAreaPage.clickYad2Btn();
+            personalAreaPage.closeWarningMessage();
             personalAreaPage.jumpAllAds();
 
         } catch (Exception e) {
